4b2d8e8f4847504751164f2b04dc44b0ab5d28e2
Iacopo Spalletti <i.spalletti@nephila.it>
2025-07-05 08:00:04 +0200

    Initial import

---
+++ wjs/themes/apps.py
@@ -0,0 +1,26 @@
+"""Configure this application."""
+
+from pathlib import Path
+
+from django.apps import AppConfig
+from django.conf import settings
+
+
+class WJSThemesConfig(AppConfig):
+    """Configuration for this django app."""
+
+    name = "wjs.themes"
+    verbose_name = "WJS Themes"
+    path = Path(__file__).parent.absolute()
+    themes = (
+        "wjs-bootstrap",
+        "JCOM-theme",
+    )
+
+    def ready(self):
+        """
+        Inject wjs-bootstrap templates into templates DIRS to make it available independently from selected theme.
+
+        wjs-review use wjs-bootstrap as a base and we must make it available globally
+        """
+        settings.TEMPLATES[0]["DIRS"].append(self.path / "wjs-bootstrap" / "templates")
