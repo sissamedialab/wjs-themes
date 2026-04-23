a5b651dd1192f952e8a4c2270922d1400b7cf632
Iacopo Spalletti <i.spalletti@nephila.it>
2026-04-23 08:50:07 +0200

    chore: remove uneeded file

--- build_assets_1.8.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-cd ../janeway-upstream/src
-echo "Compile frontend"
-python manage.py build_assets
-while inotifywait -r -e modify ../../wjs-themes/wjs/themes/JCOM-theme/assets ../../wjs-themes/wjs/themes/wjs-bootstrap/assets; do
-  python manage.py build_assets
-done
