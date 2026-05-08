f743c17e2ca4fc3cefa78017bae4401d7e71420a
Iacopo Spalletti <i.spalletti@nephila.it>
2026-05-08 03:14:05 +0200

    fix: fix tests

--- wjs/themes/management/commands/install_themes.py
+++ wjs/themes/management/commands/install_themes.py
@@ -5,7 +5,6 @@ from pathlib import Path
 from django.apps import apps
 from django.conf import settings
 from django.core.management.base import BaseCommand
-from utils.install import update_settings
 
 
 class Command(BaseCommand):
@@ -16,8 +15,6 @@ class Command(BaseCommand):
         destination_folder = (Path(settings.BASE_DIR) / "themes").resolve()
         config = apps.get_app_config("themes")
         themes_folder = config.path
-        settings_path = Path(__file__).parents[3] / "install" / "settings.json"
-        update_settings(file_path=str(settings_path))
 
         for theme in config.themes:
             destination = destination_folder / theme
